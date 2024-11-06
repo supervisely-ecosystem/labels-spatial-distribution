1aab670327d140cf4888d0bf2d553acad90d4fad
vorozhkog <vorozhkog@icloud.com>
2024-11-06 08:30:57 +0000

    fix TypeError

--- src/ui/heatmap_calculation.py
+++ src/ui/heatmap_calculation.py
@@ -80,6 +80,9 @@ def get_heatmap_image(api, state, class_name, class_idx, avg_img_size):
     cmap = plt.cm.get_cmap("viridis")
     meta_json = api.project.get_meta(g.project_info.id)
     meta = sly.ProjectMeta.from_json(meta_json)
+    if imagesCount is None:
+        sly.logger.warn(f"Images count is None. Cannot generate heatmap.")
+        return
     imagesPart = imagesCount / g.project_info.items_count
 
     if datasets_to_heatmap:
